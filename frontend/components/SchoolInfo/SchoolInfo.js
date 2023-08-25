5d875dc30e7463a311456778ea8d87c591a4692e
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-25 01:07:02 +0100

    Renamed component

---
+++ frontend/components/SchoolInfo/SchoolInfo.js
@@ -0,0 +1,88 @@
+'use client';
+
+import useSWR from 'swr';
+import Titles from '@/components/Titles/Titles';
+import Planner from '@/components/Planner/Planner';
+import BackHome from '@/components/BackHome/BackHome';
+import Mapa from '@/components/Mapa/Mapa';
+import DownloadStops from '@/components/DownloadStops/DownloadStops';
+import Pass from '@/components/Pass/Pass';
+import StopInfo from '../StopInfo/StopInfo';
+import BlackHeader from '../BlackHeader/BlackHeader';
+import { SegmentedControl } from '@mantine/core';
+import styles from './SchoolInfo.module.css';
+import { useState } from 'react';
+
+export default function SchoolInfo({ school_code }) {
+  //
+
+  //
+  // A. Setup variables
+
+  const [mapStyle, setMapStyle] = useState('map');
+
+  //
+  // B. Fetch data
+
+  const { data: schoolData } = useSWR(`https://api.carrismetropolitana.pt/facilities/schools/${school_code}`);
+
+  //
+  // C. Render components
+
+  return (
+    schoolData && (
+      <div className={styles.main}>
+        <div className={styles.titles}>
+          <Titles municipality_name={schoolData.municipality_name} school_name={schoolData.name} />
+        </div>
+
+        <div className={styles.map}>
+          <Mapa
+            id="patternShape"
+            height={500}
+            scrollZoom={false}
+            mapStyle={mapStyle}
+            toolbar={
+              <>
+                <SegmentedControl
+                  value={mapStyle}
+                  onChange={setMapStyle}
+                  size="xs"
+                  data={[
+                    { label: 'Map', value: 'map' },
+                    { label: 'Satellite', value: 'satellite' },
+                  ]}
+                />
+              </>
+            }
+            latitude={schoolData.lat}
+            longitude={schoolData.lon}
+            schoolInfo={schoolData}
+            // stops={stops}
+          ></Mapa>
+        </div>
+
+        <div className={styles.stops}>
+          <BlackHeader text={`Paragens que servem a instituição: ${schoolData.name}`} />
+          <div className={styles.listOfStops}>
+            {schoolData.stops.map((stopCode) => (
+              <StopInfo key={stopCode} stop_code={stopCode} />
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.info}>
+          <DownloadStops schoolInfo={schoolData} municipality={schoolData ? schoolData.municipality_name : schoolData} school={schoolData} stops={false ? false : []} />
+          <Planner />
+          <Pass />
+        </div>
+
+        <div className={styles.back}>
+          <BackHome />
+        </div>
+      </div>
+    )
+  );
+
+  //
+}

9bfb558a788b53009422da738fe68d4a1fb7eb7a
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-25 02:38:15 +0100

    Test 2

---
+++ frontend/.puppeteerrc.cjs
@@ -0,0 +1,12 @@
+// Set .cache to root of container
+// https://pptr.dev/
+
+const { join } = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};

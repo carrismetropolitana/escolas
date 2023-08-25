0f61a8b292591362cbc7316e2d6a5fa4660e67e5
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-08-25 01:03:29 +0100

    Removed ExpressJS in favor of NextJS built-in router
    https://nextjs.org/docs/pages/building-your-application/routing/api-routes

--- frontend/pages/api/pdfGenerator.js
+++
@@ -1,7 +0,0 @@
-// pages/api/pdfGenerator.js
-
-// http://localhost:5051/generate-pdf?url=https://menthadigital.com/
-
-import { pdfGenerator } from '../../utils/pdfGenerator';
-
-export default pdfGenerator;
